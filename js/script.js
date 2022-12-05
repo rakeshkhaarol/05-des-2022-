82050eaf7f45e674ca3188e5b3d48115edb8a36f
rakeshkhaarol <rakeshkharol457@gmail.com>
2022-12-05 21:32:05 +0530

    4 news completed

---
+++ js/script.js
@@ -0,0 +1,48 @@
+//JS Object for storing student informatino
+
+var news1 ={
+    //property:velue
+
+    n1:"Prince",
+    n2:"Harry-Meghan",
+    n3:"Markle:",
+    n4:" Earlier,",
+    n5:"Prince",
+    n6:" Harry ",
+    n7:"and",
+    n8:" Meghan",
+    n9:" Markle",
+    n10:" released ",
+    n11:"a",
+    n12:" dramatic",
+    n13:" one-minute",
+    n14:" trailer",
+    n15:" on ",
+    n16:"social",
+    n17:" media ",
+    n18:" of ",
+    n19:" the ",
+    n20:"series.",
+    n21:"Prince ",
+    n22:"Harry-Meghan ",
+    n23:" Markle: ",
+    n24:" Prince ",
+    n25:"series.",
+    n26:"Harry ",
+    n27:"and",
+    n28:" Meghan",
+    n29:"Markle,",
+    n30:"Duke",
+    n31:"and",
+    n32:"Duchess",
+    n33:"of",
+    n34:"Sussex ",
+    n35:"are",
+    n36:"seen.",
+    n37:"(AP)"
+}
+
+var s = news1.n1+" "+news1.n2+" "+news1.n3+""+news1.n4+""+news1.n5+""+news1.n6+""+news1.n7+" "+news1.n8+""+news1.n9+""+news1.n10+""+news1.n11+""+news1.n12+""+news1.n13+""+news1.n14+""+news1.n15+""+news1.n16+""+news1.n17+""+news1.n18+""+news1.n19+""+news1.n20+""+news1.n21+""+news1.n22+""+news1.n23+""+news1.n24+""+news1.n25+""+news1.n26+""+news1.n27+""+news1.n28+""+news1.n29+""+news1.n30+""+news1.n31+""+news1.n32+""+news1.n33+""+news1.n34+""+news1.n35+""+news1.n36+""+news1.n37
+
+//outpute see 4 ways first is a console.log
+console.log(s)
